e783fc6e60077dd79daf080951e9c3f2fb988254
mzw111 <zaidwadood15723@gmail.com>
2026-02-26 17:43:25 +0530

    Fixed backend to run HTTP server on port 8080

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-#include <cstdlib>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <cstring>
 
 int main() {
-    std::cout << "Served by backend: " << getenv("HOSTNAME") << std::endl;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend: ";
+        response += getenv("HOSTNAME");
+        response += "\n";
+
+        send(new_socket, response.c_str(), response.length(), 0);
+        close(new_socket);
+    }
+
     return 0;
 }
